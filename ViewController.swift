66a649d8791e727606cf885a3b1a684ad6d483dc
wamuni <boshi.pan@outlook.com>
2024-04-09 10:03:08 +0100

    add button to main viewcontroller

--- ViewController.swift
+++ ViewController.swift
@@ -14,10 +14,21 @@ struct Course {
 class ViewController: UIViewController {
     
     let courses = [Course]()
+    
+    private var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Tap me", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.frame = CGRect(x: 0, y: 0, width: 250, height: 50)
+        button.backgroundColor = .systemBlue
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(button)
+        button.center = view.center
     }
     
     // to get api response of a list of course
