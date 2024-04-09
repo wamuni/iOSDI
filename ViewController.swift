5fb7dc510e64b204fc32564a4287dfe9e482d7d5
wamuni <boshi.pan@outlook.com>
2024-04-09 09:20:33 +0100

    set up project for DI pattern for iOS

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DI
+//
+//  Created by 潘博石 on 09/04/2024.
+//
+
+import UIKit
+
+struct Course {
+    var name: String
+}
+
+class ViewController: UIViewController {
+    
+    let courses = [Course]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // to get api response of a list of course
+    // https://iosacademy.io/api/v1/courses/index.php
+
+}
+
